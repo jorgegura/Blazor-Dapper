6f09c65b8c97414059d8e184c05209878c9cbe97
jorgegura <59863985+jorgegura@users.noreply.github.com>
2020-06-11 12:03:49 +0200

    Actualizacion1.0

---
+++ BlazorCRUB/BlazorCRUB.Data.Dapper/Repositories/FilmRepository.cs
@@ -0,0 +1,55 @@
+﻿using BlazorCRUB.Model;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUB.Data.Dapper.Repositories
+{
+    public class FilmRepository : IFilmRepository
+    {
+
+        private string ConnectionString;
+
+        public FilmRepository(string connectoonString)
+        {
+            ConnectionString = connectoonString;
+        }
+
+        protected SqlConnection dbConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+
+        public Task<bool> DeleteFilm(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<Film>> GetAllFilms()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Film> GetFilmDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<bool> InsertFilm(Film film)
+        {
+            var db = dbConnection();
+
+            var sql = @"INSER INTO Films (Title,Director, ReleaseDate)
+                        VALUES (@Title, @Director, @ReleaseDate)";
+
+            var result = await db.ExecuteAsync(sql.ToString(),
+                new { film.Title, film.Director, film.ReleaseDate });
+
+            return result > 0;
+        }
+        public Task<bool> UpdateFilm(Film film)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
