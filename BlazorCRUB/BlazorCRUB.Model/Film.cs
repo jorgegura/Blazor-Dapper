7b97e8e79779c95267f79732f9d71c95e390d5f3
jorgegura <59863985+jorgegura@users.noreply.github.com>
2020-06-11 11:26:39 +0200

    conexion xcon la DB

---
+++ BlazorCRUB/BlazorCRUB.Model/Film.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorCRUB.Model
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
